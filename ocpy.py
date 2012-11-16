6ff965eceaba273f578646f5751ece07bd25d5b5
Michael Göhler <somebody.here@gmx.de>
2012-11-16 22:19:51 +0100

    initial commit

---
+++ ocpy.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python
+#
+#   Copyright (c) 2012 by Michael Goehler <somebody.here@gmx.de>
+#
+#   This file is part of ocpy.
+#
+#   ocpy is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   ownCloudTray is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with ocpy. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys, os, time, atexit, pyinotify, subprocess, threading
+from signal import SIGTERM 
+
+class Daemon:
+	"""
+	A generic daemon class.
+
+	Many thanks to Sander Marechal
+	http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
+	"""
+	def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+		self.stdin = stdin
+		self.stdout = stdout
+		self.stderr = stderr
+		self.pidfile = pidfile
+	
+	def daemonize(self):
+		"""
+		do the UNIX double-fork magic, see Stevens' "Advanced 
+		Programming in the UNIX Environment" for details (ISBN 0201563177)
+		http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
+		"""
+		try: 
+			pid = os.fork() 
+			if pid > 0:
+				# exit first parent
+				sys.exit(0) 
+		except OSError, e: 
+			sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+			sys.exit(1)
+	
+		# decouple from parent environment
+		os.chdir("/") 
+		os.setsid() 
+		os.umask(0) 
+	
+		# do second fork
+		try: 
+			pid = os.fork() 
+			if pid > 0:
+				# exit from second parent
+				sys.exit(0) 
+		except OSError, e: 
+			sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+			sys.exit(1) 
+	
+		# redirect standard file descriptors
+		sys.stdout.flush()
+		sys.stderr.flush()
+		si = file(self.stdin, 'r')
+		so = file(self.stdout, 'a+')
+		se = file(self.stderr, 'a+', 0)
+		os.dup2(si.fileno(), sys.stdin.fileno())
+		os.dup2(so.fileno(), sys.stdout.fileno())
+		os.dup2(se.fileno(), sys.stderr.fileno())
+	
+		# write pidfile
+		atexit.register(self.delpid)
+		pid = str(os.getpid())
+		file(self.pidfile,'w+').write("%s\n" % pid)
+	
+	def delpid(self):
+		os.remove(self.pidfile)
+
+	def start(self):
+		"""
+		Start the daemon
+		"""
+		# Check for a pidfile to see if the daemon already runs
+		try:
+			pf = file(self.pidfile,'r')
+			pid = int(pf.read().strip())
+			pf.close()
+		except IOError:
+			pid = None
+	
+		if pid:
+			message = "pidfile %s already exist. Daemon already running?\n"
+			sys.stderr.write(message % self.pidfile)
+			sys.exit(1)
+		
+		# Start the daemon
+		self.daemonize()
+		self.run()
+
+	def stop(self):
+		"""
+		Stop the daemon
+		"""
+		# Get the pid from the pidfile
+		try:
+			pf = file(self.pidfile,'r')
+			pid = int(pf.read().strip())
+			pf.close()
+		except IOError:
+			pid = None
+	
+		if not pid:
+			message = "pidfile %s does not exist. Daemon not running?\n"
+			sys.stderr.write(message % self.pidfile)
+			return # not an error in a restart
+
+		# Try killing the daemon process	
+		try:
+			while 1:
+				os.kill(pid, SIGTERM)
+				time.sleep(0.1)
+		except OSError, err:
+			err = str(err)
+			if err.find("No such process") > 0:
+				if os.path.exists(self.pidfile):
+					os.remove(self.pidfile)
+			else:
+				print str(err)
+				sys.exit(1)
+
+	def restart(self):
+		"""
+		Restart the daemon
+		"""
+		self.stop()
+		self.start()
+
+	def run(self):
+		"""
+		Our subclass will override this method. It will be called after the process has been
+		daemonized by start() or restart().
+		"""
+
+
+
+class ocpy(Daemon, pyinotify.ProcessEvent):
+	"""
+	The ocpy daemon
+	"""
+	def watch(self, directory):
+		"""
+		add a directory to inotify
+		"""
+		self.watchdesc = self.watchman.add_watch(directory, self.mask, rec=True)
+
+
+	def unwatch(self):
+		"""
+		remove all directories from inotify
+		"""
+		self.watchman.rm_watch(self.watchdesc.values(), rec=True)
+
+
+	def newThread(self, callback, args):
+		"""
+		child process to spawn
+		"""
+		#self.csyncProc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		self.csyncProc = subprocess.Popen(args)
+		self.csyncProc.wait()
+		callback(self.csyncProc.returncode)
+
+
+	def cbThread(self, returncode):
+		"""
+		callback of the child process
+		"""
+		self.csyncInProgress = False
+
+		if self.csyncSubmitAgain == True:
+			self.sync()
+		else:
+			self.csyncTimer = threading.Timer(self.csyncTimeout, self.sync)
+			self.csyncTimer.start()
+
+
+	def sync(self):
+		"""
+		main synchronization method
+		"""
+		if self.csyncInProgress == False:
+			self.csyncSubmitAgain = False
+			self.csyncInProgress = True
+
+			# create the csync command
+			csyncArgs = [self.csyncExe, self.csyncLocalPath, self.csyncProtocol + '://' + self.csyncUser + ':' + self.csyncPassword + '@' + self.csyncHost + ':' + str(self.csyncPort) + self.csyncRemotePath + '/' + self.csyncSubfolder]
+
+			# start csync
+			self.csyncThread = threading.Thread(target=self.newThread, args=(self.cbThread, csyncArgs))
+			self.csyncThread.start()
+		else:
+			self.csyncSubmitAgain = True
+
+
+	def process_IN_CREATE(self, event):
+		"""
+		inotify event callback
+		"""
+		if event.name != '.csync_timediff.ctmp':
+			self.sync()
+
+
+	def process_IN_DELETE(self, event):
+		"""
+		inotify event callback
+		"""
+		if event.name != '.csync_timediff.ctmp':
+			self.sync()
+
+
+	def process_IN_MODIFY(self, event):
+		"""
+		inotify event callback
+		"""
+		if event.name != '.csync_timediff.ctmp':
+			self.sync()
+
+
+	def process_IN_MOVED_FROM(self, event):
+		"""
+		inotify event callback
+		"""
+		if event.name != '.csync_timediff.ctmp':
+			self.sync()
+
+
+	def process_IN_MOVED_TO(self, event):
+		"""
+		inotify event callback
+		"""
+		if event.name != '.csync_timediff.ctmp':
+			self.sync()
+
+
+	def run(self):
+		"""
+		superclass start point
+		"""
+                self.name = 'ocpy'
+                self.version = '0.1.0'
+
+                self.csyncExe        = '/usr/bin/csync'
+                self.csyncLocalPath  = '/home/visit/owncloud'
+                self.csyncProtocol   = 'ownclouds'
+                self.csyncUser       = 'myuser'
+                self.csyncPassword   = 'mypass'
+                self.csyncHost       = 'mycloud.de'
+                self.csyncPort       = 443 
+                self.csyncRemotePath = '/files/webdav.php'
+                self.csyncSubfolder  = 'clientsync'
+                self.csyncTimeout    = 60
+
+                # internal status flags
+                self.csyncInProgress = False
+                self.csyncSubmitAgain = False
+
+		# initialize inotify
+		self.watchman = pyinotify.WatchManager()
+		self.watchdesc = None
+		self.mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY | pyinotify.IN_MOVED_FROM | pyinotify.IN_MOVED_TO
+		self.notifier = pyinotify.ThreadedNotifier(self.watchman, self)
+		self.notifier.start()
+
+		# watch directory
+		self.watch(self.csyncLocalPath)
+
+		# sync
+		self.sync()
+
+
+
+if __name__ == "__main__":
+	daemon = ocpy('/tmp/ocpy.pid', '/dev/null', '/tmp/ocpy.log', '/tmp/ocpy.log')
+	if len(sys.argv) == 2:
+		if 'start' == sys.argv[1]:
+			daemon.start()
+		elif 'stop' == sys.argv[1]:
+			daemon.stop()
+		elif 'restart' == sys.argv[1]:
+			daemon.restart()
+		elif 'run' == sys.argv[1]:
+			daemon.run()
+		else:
+			print "Unknown argument: %s" % sys.argv[1]
+			sys.exit(2)
+		sys.exit(0)
+	else:
+		print "usage: %s start|stop|restart|run" % sys.argv[0]
+		sys.exit(2)
+
